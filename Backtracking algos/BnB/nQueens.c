8959ee7d3efb02ab3e9281bafb975638b5555eec
Sphoorti Nipanikar <109842862+Sphoorti18@users.noreply.github.com>
2024-04-29 10:58:26 +0530

    Update nQueens.c
    Maximizing the profits!

--- Backtracking algos/BnB/nQueens.c
+++ Backtracking algos/BnB/nQueens.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #define MAX 100
+int n,m; // n = # of profits + weights, m = Max capacity 
 //NOT defining MACROS coz it will lead to unexpected behavior by the computer
 int p[MAX][MAX], w[MAX][MAX], s[MAX][MAX], x[MAX][MAX];
 int main(){
