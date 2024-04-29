1b6143bf2c766ab09b4f123adee995dd9d0f9b2c
Sphoorti Nipanikar <109842862+Sphoorti18@users.noreply.github.com>
2024-04-29 11:16:26 +0530

    Update nQueens.c
    I missed somthing, coming back to it

--- Backtracking algos/BnB/nQueens.c
+++ Backtracking algos/BnB/nQueens.c
@@ -3,12 +3,25 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #define MAX 100
+#define new printf("\n");
 int n,m; // n = # of profits + weights, m = Max capacity 
 //NOT defining MACROS coz it will lead to unexpected behavior by the computer
 int p[MAX][MAX], w[MAX][MAX], s[MAX][MAX], x[MAX][MAX]; //Refer to hola.txt
 int main(){ int n,m,i,j;
 printf("Enter the # of profits and weights : "); scanf("%d", &n);
 printf("Enter the Max capacity : "); scanf("%d", &m);
-
+new
+  printf("Enter each profits and weights, Here : \n");
+for(i=0;i<n;i++){
+  for(j=0;j<n;j++){
+    scanf("%d %d",&p[i][j], &w[i][j]);
+  }
+}
+  printf("Enter each profits and weights, Here : \n");
+  for(i=0;i<m;i++){
+  for(j=0;j<m;j++){
+    scanf("%d %d",&p[i][j], &w[i][j]);
+  }
+}         
   return 0;
 }
