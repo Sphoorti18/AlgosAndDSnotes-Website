8c332c1b4dde430e8fde7955d0cce7f283d821e9
Sphoorti Nipanikar <109842862+Sphoorti18@users.noreply.github.com>
2024-04-29 11:02:52 +0530

    Update nQueens.c
    None ;)

--- Backtracking algos/BnB/nQueens.c
+++ Backtracking algos/BnB/nQueens.c
@@ -1,12 +1,13 @@
-//TIME COMPLEXITY ====> O(n)
+//TIME COMPLEXITY ====> O(n); REFER TO HOLA.TXT
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #define MAX 100
 int n,m; // n = # of profits + weights, m = Max capacity 
 //NOT defining MACROS coz it will lead to unexpected behavior by the computer
-int p[MAX][MAX], w[MAX][MAX], s[MAX][MAX], x[MAX][MAX];
+int p[MAX][MAX], w[MAX][MAX], s[MAX][MAX], x[MAX][MAX]; //Refer to hola.txt
 int main(){
-
+printf("Enter the # of profits and weights : "); scanf("%d", &n);
+printf("Enter the Max capacity : "); scanf("%d", &m);
   return 0;
 }
