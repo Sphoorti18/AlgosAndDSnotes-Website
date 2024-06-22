a5d6fcdf1d929e488a6307f515982f5fbf710945
Sphoorti Nipanikar <109842862+Sphoorti18@users.noreply.github.com>
2024-06-22 18:51:44 +0530

    Create forward.c

---
+++ DYNAMIC PROGRAMMING/FORWARD/forward.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// Function to print an array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printf("Array elements in forward order: \n");
+    printArray(arr, n);
+    return 0;
+}
